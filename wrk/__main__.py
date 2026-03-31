15f082981c0b19348e24659dc1454bc1db8394e8
Peter Sutton <peter@foxdogstudios.com>
2026-03-31 16:11:25 +0100

    Clear command

--- wrk/__main__.py
+++ wrk/__main__.py
@@ -2,14 +2,18 @@ from argparse import ArgumentParser
 
 from wrk._report import print_report
 from wrk._ui import run_ui
+from wrk._database import open_database, transaction
 
 
 def main() -> None:
     parser = ArgumentParser()
-    parser.add_argument('command', nargs='?', choices=('report',))
+    parser.add_argument('command', nargs='?', choices=('report','clear'))
     args = parser.parse_args()
     if args.command == 'report':
         print_report()
+    elif args.command == 'clear':
+        with open_database() as conn, transaction(conn) as cursor:
+            cursor.execute('DELETE FROM events')
     else:
         run_ui()
 
